9cf420a30e06ab656eaa13429734d37bf7d52e0c
seba acuña <seba.nobody@gmail.com>
2020-02-14 22:54:01 -0300

    avance sync automaticos

--- app/src/main/java/cl/smapdev/curimapu/MainActivity.java
+++ app/src/main/java/cl/smapdev/curimapu/MainActivity.java
@@ -12,6 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.room.Room;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -48,10 +50,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     public static MyAppBD myAppDB;
 
+    // Constants
+    // The authority for the sync adapter's content provider
+    public static final String AUTHORITY = "cl.smapdev.curimapu.clases.StubProvider";
+    // An account type, in the form of a domain name
+    public static final String ACCOUNT_TYPE = "smapdev.cl";
+    // The account name
+    public static final String ACCOUNT = "dummyaccount";
+    // Instance fields
+    Account mAccount;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        // Create the dummy account
+        mAccount = CreateSyncAccount(this);
 
 //        .addMigrations(Migrations.MIGRATION_1_TO_2,Migrations.MIGRATION_2_TO_3).
         myAppDB = Room.databaseBuilder(getApplicationContext(),MyAppBD.class,"curimapu.db").allowMainThreadQueries().build();
@@ -117,6 +133,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+
+        return newAccount;
+    }
 
     public void setDrawerEnabled(boolean enabled) {
         int lockMode = enabled ? DrawerLayout.LOCK_MODE_UNLOCKED :
---
+++ app/src/main/java/cl/smapdev/curimapu/clases/AuthenticatorService.java
@@ -0,0 +1,33 @@
+package cl.smapdev.curimapu.clases;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class AuthenticatorService extends Service {
+
+    private Authenticator mAuthenticator;
+
+    // Instance field that stores the authenticator object
+    @Override
+    public void onCreate() {
+
+        // Create a new authenticator object
+        mAuthenticator = new Authenticator(this);
+    }
+
+
+    /*
+     * When the system binds to this Service to make the RPC call
+     * return the authenticator's IBinder.
+     */
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+
+
+}
